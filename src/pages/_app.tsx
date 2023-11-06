9c650d932fa41a5c7f5cd2db7f14de93b90edc3b
Brad Gibbs <bradley@thegyre.io>
2023-11-06 16:37:41 -0600

    more font issues;

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,25 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-import Geist from "next/font/local";
-import { ThemeProvider } from "next-themes";
-import Layout from "@/components/Layout";
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+import Geist from 'next/font/local'
+import { ThemeProvider } from 'next-themes'
+import Layout from '@/components/Layout'
 
 const geist = Geist({
   src: [
-    { path: "./fonts/Geist-Thin.woff2", weight: "100" },
-    { path: "./fonts/Geist-Ultralight.woff2", weight: "200" },
-    { path: "./fonts/Geist-Light.woff2", weight: "300" },
-    { path: "./fonts/Geist-Regular.woff2", weight: "400" },
-    { path: "./fonts/Geist-Medium.woff2", weight: "500" },
-    { path: "./fonts/Geist-Semibold.woff2", weight: "600" },
-    { path: "./fonts/Geist-Bold.woff2", weight: "700" },
-    { path: "./fonts/Geist-Black.woff2", weight: "800" },
-    { path: "./fonts/Geist-UltraBlack.woff2", weight: "900" },
+    { path: './fonts/Geist-Thin.woff2', weight: '100' },
+    { path: './fonts/Geist-UltraLight.woff2', weight: '200' },
+    { path: './fonts/Geist-Light.woff2', weight: '300' },
+    { path: './fonts/Geist-Regular.woff2', weight: '400' },
+    { path: './fonts/Geist-Medium.woff2', weight: '500' },
+    { path: './fonts/Geist-SemiBold.woff2', weight: '600' },
+    { path: './fonts/Geist-Bold.woff2', weight: '700' },
+    { path: './fonts/Geist-Black.woff2', weight: '800' },
+    { path: './fonts/Geist-UltraBlack.woff2', weight: '900' },
   ],
-  display: "swap",
-  variable: "--font-geist-sans",
+  display: 'swap',
+  variable: '--font-geist-sans',
   // fallback: ["inter"],
-});
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -30,5 +30,5 @@ export default function App({ Component, pageProps }: AppProps) {
         </Layout>
       </main>
     </ThemeProvider>
-  );
+  )
 }
